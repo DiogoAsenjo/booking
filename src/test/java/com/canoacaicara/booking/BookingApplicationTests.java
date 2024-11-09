000c0d7a9174545b00b6c265f20c186dc834bfff
Diogo Asenjo <diogoasenjo@outlook.com>
2024-11-09 18:20:13 -0300

    Starting project

---
+++ src/test/java/com/canoacaicara/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canoacaicara.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

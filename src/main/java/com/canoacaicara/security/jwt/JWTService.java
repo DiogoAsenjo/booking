f8978dbcc08c4a832f8e6c3fc0913a787a5ba56c
Diogo Asenjo <diogoasenjo@outlook.com>
2025-02-11 14:38:00 -0300

    Ultimas mudanças após inclusão do JWT

--- src/main/java/com/canoacaicara/security/jwt/JWTService.java
+++ src/main/java/com/canoacaicara/security/jwt/JWTService.java
@@ -3,12 +3,16 @@ package com.canoacaicara.security.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
 
 public class JWTService {
-    private String SECRET_KEY = "123";
-    private long EXPIRATION_TIME = 3600000;
+    @Value("${jwt.secret-key}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.expiration-time}")
+    private long EXPIRATION_TIME;
 
     public String generateToken(String email) {
         return JWT.create()
